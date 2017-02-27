eaac3f9727e8780db7a75893c3e4b501a6191b63
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-02-27 14:09:15 +0800

    update reset password test

--- tests/ResetPasswordTest.php
+++ tests/ResetPasswordTest.php
@@ -14,9 +14,10 @@ class ResetPasswordTest extends TestCase
         $this->email = 'john@doe.com';
         $this->password = '123456';
         $this->confirmPassword = '123456';
+        $this->token = 'test123';
     }
 
-    public function testViewPasswordReset()
+    public function testViewForgotPassword()
     {
         $this->visit('/')
             ->see('Welcome')
@@ -54,8 +55,58 @@ class ResetPasswordTest extends TestCase
             ->see('We have e-mailed your password reset link!');
     }
 
-    public function testShouldReceiveEmail()
+    public function testResetPasswordWithoutToken()
     {
-        
+        $this->visit('/password/reset')
+            ->see('Reset Password');
+    }
+
+    public function testResetPasswordWithWrongToken()
+    {
+        $this->visit('/password/reset/' . $this->token)
+            ->see('Reset Password')
+            ->type('test@test.com', 'email')
+            ->type($this->password, 'password')
+            ->type($this->confirmPassword, 'password_confirmation')
+            ->press('Reset Password')
+            ->seePageIs('/password/reset/' . $this->token)
+            ->see('We can\'t find a user with that e-mail address.');
+    }
+
+    public function testResetPasswordWithCorrectToken()
+    {
+        $this->visit('/register')
+            ->see('Register')
+            ->type($this->name, 'name')
+            ->type($this->email, 'email')
+            ->type($this->password, 'password')
+            ->type($this->confirmPassword, 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('/home')
+            ->see('Dashboard')
+            ->click('Logout')
+            ->seePageIs('/')
+            ->see('Welcome')
+            ->click('Login')
+            ->seePageIs('/login')
+            ->see('Login')
+            ->click('Forgot Your Password?')
+            ->seePageIs('/password/reset')
+            ->see('Reset Password')
+            ->type($this->email, 'email')
+            ->press('Send Password Reset Link')
+            ->seePageIs('/password/reset')
+            ->see('We have e-mailed your password reset link!');
+
+        $data = DB::table('password_resets')->first();
+
+        $this->visit('/password/reset/' . $data->token)
+            ->see('Reset Password')
+            ->type($data->email, 'email')
+            ->type($this->password, 'password')
+            ->type($this->confirmPassword, 'password_confirmation')
+            ->press('Reset Password')
+            ->seePageIs('/home')
+            ->see('Dashboard');
     }
 }
